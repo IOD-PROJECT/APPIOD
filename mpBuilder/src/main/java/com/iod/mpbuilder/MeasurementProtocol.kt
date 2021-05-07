37dc056e1876de20bf6accd3053a3461eca4026e
NovandoS <50234225+IlmuwanData@users.noreply.github.com>
2021-05-07 14:19:20 +0700

    Initial commit

---
+++ mpBuilder/src/main/java/com/iod/mpbuilder/MeasurementProtocol.kt
@@ -0,0 +1,36 @@
+package com.iod.mpbuilder
+
+class MeasurementProtocol {
+    companion object Param{
+        val CURRENCY : String = "item_currency"
+        val CUSTOM_DIMENSION_1 : String = "custom_dimension1"
+        val TRANSACTION_ID : String = "ti"
+        val TRANSACTION_AFFILIATION : String = "ta"
+        val TRANSACTION_REVENUE : String = "tr"
+        val TRANSACTION_SHIPPING : String = "ts"
+        val TRANSACTION_TAX : String = "tt"
+        val TRANSACTION_COUPON : String = "tcc"
+        val VERSION : String = "v"
+        val APP_VERSION : String = "av"
+        val TRACKING_ID : String = "tid"
+        val CLIENT_ID : String = "cid"
+        val HIT_TYPE : String = "t"
+        val APP_NAME : String = "an"
+        val APP_ID : String = "aid"
+        val EVENT_ACTION : String = "ea"
+        val EVENT_CATEGORY : String = "ec"
+        val EVENT_LABEL : String = "el"
+        val PRODUCT_ACTION : String = "pa"
+        val PRODUCT_POSITION : String = "item_position"
+        val ITEM_ID : String = "item_id"
+        val ITEM_NAME : String = "item_name"
+        val ITEM_BRAND : String = "item_brand"
+        val ITEM_PRICE : String = "item_price"
+        val ITEM_CATEGORY : String = "item_category"
+        val ITEM_VARIANT : String = "item_variant"
+        val ITEM_POSITION : String = "item_position"
+        val ITEM_QUANTITY : String = "item_quantity"
+        val CUSTOM_DIMENSIONTEST : String = "custom_dimension2"
+
+    }
+}
